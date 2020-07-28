35dc727914055bbe7457d9a51899f7db66b38830
Miguel Cardoso <msdc.1000@hotmail.com>
2020-07-28 16:28:32 +0100

    Added scripts to generate n-size task
    *Generate n-size task.
    *Attention check added
    *Demographic questions added

---
+++ task/generation/generate_tasks.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Generate Annotate and Verify tasks with n comments.
+@author: Miguel Cardoso
+"""
+
+#Parameters
+n = 5 # NUMBER OF COMMENTS IN A TASK
+start_attention_check = 1
+interval_attention_check = 3
+
+#--------------------------------
+attention_check = interval_attention_check
+attention_number = 0
+annotate = open("annotate-task-template.html", "r").read()
+verify = open("verify-task-template.html","r").read()
+attention = open("attention-check.html","r").read()
+task = open("task-template.html", "r").read().replace("{n}",str(n+1))
+
+final_annotate = task
+final_verify = task
+body_annotate = ""
+body_verify = ""
+for i in range(n):
+    if(i == start_attention_check or attention_check == 0):
+        body_verify = body_verify + attention.replace("{n}",str(i+1)).replace("{an}",str(attention_number+1))
+        body_annotate = body_annotate + attention.replace("{n}",str(i+1)).replace("{an}",str(attention_number+1))
+
+        attention_check = interval_attention_check
+        attention_number = attention_number + 1
+    else:
+        body_annotate = body_annotate + annotate.replace("{n}",str(i+1)).replace("{ln}",str(i+1-attention_number))
+        body_verify = body_verify + verify.replace("{n}",str(i+1)).replace("{ln}",str(i+1-attention_number))
+        attention_check = attention_check - 1
+final_annotate = final_annotate.replace("{TASK}",body_annotate)
+
+f = open("annotate-task-"+str(n)+".html", "w+")
+f.write(final_annotate)
+f.close()
+
+final_verify = final_verify.replace("{TASK}",body_verify)
+f = open("verify-task-"+str(n)+".html", "w+")
+f.write(final_verify)
+f.close()
